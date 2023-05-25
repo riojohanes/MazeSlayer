1df589bc558b5b3facf9a11ca2f9a3b556e4260b
Rio Johanes <72259697+riojohanes@users.noreply.github.com>
2023-05-25 14:54:53 +0700

    Initial Commit

---
+++ MazeSlayer/MazeSlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MazeSlayerApp.swift
+//  MazeSlayer
+//
+//  Created by Rio Johanes Sumolang on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MazeSlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
